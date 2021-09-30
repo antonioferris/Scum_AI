df5f1a56df44237a6b44cf73c19c1a88289fbd8a
Antonio Ferris <antonioferris01@gmail.com>
2021-09-30 12:47:08 -0700

    Adding Profiler to profile code because code was slow ... because of an infinite loop bug

---
+++ Profiler.py
@@ -0,0 +1,23 @@
+import cProfile
+from pstats import Stats, SortKey
+import Scum
+
+
+def run_script():
+    Scum.test_random_agents(5, 1000, 1000)
+
+
+if __name__ == '__main__':
+    do_profiling = True
+    if do_profiling:
+        with cProfile.Profile() as pr:
+            run_script()
+
+        with open('profiling_stats.txt', 'w') as stream:
+            stats = Stats(pr, stream=stream)
+            stats.strip_dirs()
+            stats.sort_stats('time')
+            stats.dump_stats('.prof_stats')
+            stats.print_stats()
+    else:
+        run_script()
